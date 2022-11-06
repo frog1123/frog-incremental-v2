f0f9a036b63aebb223f9f90f62de1a13da9db346
frog1123 <93895449+frog1123@users.noreply.github.com>
2022-11-05 20:41:06 -0500

    add sidebar

--- src/scripts/core/ui.js
+++ src/scripts/core/ui.js
@@ -12,6 +12,29 @@ const updateUI = () => {
   setTimeout(updateUI, 50);
 };
 
+const switchPage = pageToSwitchTo => {
+  // hide all pages
+  document.getElementById('page-main').style.display = 'none';
+  document.getElementById('page-2').style.display = 'none';
+
+  document.getElementById(pageToSwitchTo).style.display = 'block';
+};
+
+// sidebar open
+document.getElementById('sidebar-icon-btn').onclick = () => {
+  document.getElementById('sidebar-container').style.transform = 'translateX(0)';
+  document.getElementById('sidebar-container').style.boxShadow = '0 8px 24px #191919';
+};
+
+// sidebar close
+document.getElementById('sidebar-close-btn').onclick = () => {
+  document.getElementById('sidebar-container').style.transform = 'translateX(-100%)';
+  document.getElementById('sidebar-container').style.boxShadow = 'none';
+};
+
+document.getElementById('switch-to-page-main').onclick = () => switchPage('page-main');
+document.getElementById('switch-to-page-2').onclick = () => switchPage('page-2');
+
 document.getElementById('frog-pond-multiplier-buy-btn').onclick = () => upgradeMultiplier();
 document.getElementById('pond-t1-buy-btn').onclick = () => upgradePond(1);
 document.getElementById('pond-t2-buy-btn').onclick = () => upgradePond(2);
